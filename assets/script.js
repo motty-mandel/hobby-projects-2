9528437dc702f8d61611058133579a6820fd2b6a
motty mandel <mandelmottyisrael65@gmail.com>
2023-07-17 16:42:55 -0400

    started working on the javaScript of the project

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var min = 8;
+var max = 128;
+
+const symbols = ("!#$%&'() *+,-./")
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    let password = document.querySelector('#password');
 
-  passwordText.value = password;
+    let passwordLength = prompt("How long do you want your password to be? Min. amount 8, max. amount 128.");
+    if (passwordLength >= 8 && passwordLength <= 128) {
+        console.log("length is good")
+    } else if (passwordLength === null) {
+        return;
+    } else {
+        return;
+    }
+
+    let addUpper = confirm("Do you want to include uppercase letters?");
+    if (addUpper) {
+        const upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        const upperArray = upperCase.split("");
+
+        document.getElementById("password").innerHTML = upperArray[Math.floor(Math.random() * upperCase.length)];
+    } else {
+        console.log("no uppercase");
+    }
+
+    let addLower = confirm("Do you want to include lowercase letters?");
+    if (addLower) {
+        const lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+        const lowerArray = lowerCase.split("");
+
+        document.getElementById("password").innerHTML = lowerArray[Math.floor(Math.random() * lowerCase.length)];
+    } else {
+        console.log("no lowercase");
+    }
+    
+    let addNumbers = confirm("Do you want to include numbers?");
+    if (addNumbers) {
+        const numbers = ("0123456789");
+        const numbersArray = numbers.split("");
+
+        document.getElementById("password").innerHTML = numbersArray[Math.floor(Math.random() * numbers.length)];
+    } else {
+        console.log("no numbers");
+    }
+    
+    let addSymbols = confirm("Do you want to include symbols?");
+    if (addSymbols) {
+        const symbols = ("!#$%&'() *+,-./");
+        const symbolsArray = symbols.split("");
+
+        document.getElementById("password").innerHTML = symbolsArray[Math.floor(Math.random() * symbols.length)];
+        
+    } else {
+        console.log("no symbols");
+    }
+
+    return;
 
 }
 
